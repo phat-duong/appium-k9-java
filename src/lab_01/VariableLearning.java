c84406c800d679672d9870ce5121e04150ffae7b
Duong Tan Phat <phat.duong.henry@gmail.com>
2022-05-21 20:52:18 +0700

    Lab 01

---
+++ src/lab_01/VariableLearning.java
@@ -0,0 +1,16 @@
+package lab_01;
+
+public class VariableLearning {
+    public static void main(String[] args) {
+        byte myByteNumber = 1;
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Short.MAX_VALUE);
+        System.out.println(Short.MIN_VALUE);
+        System.out.println(myByteNumber);
+
+        float myFloatNumber = 2.1f;
+        double myDoubleNumber = 2.2d;
+        long myLongNumber = 123123123L;
+
+    }
+}
