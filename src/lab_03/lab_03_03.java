c1bfe36e5d2b6b1070f9d336bdb019987d6039ae
Duong Tan Phat <phat.duong.henry@gmail.com>
2022-05-31 00:00:54 +0700

    Update lab 03_03

--- src/lab_03/lab_03_03.java
+++ src/lab_03/lab_03_03.java
@@ -6,7 +6,16 @@ public class lab_03_03 {
     public static void main(String[] args) {
         int[] arr = {12, 34, 1, 16, 28};
 
-        Arrays.sort(arr);
+        int temp;
+            for (int i = 0 ; i < arr.length - 1; i++) {
+                for (int j = i + 1; j < arr.length; j++) {
+                    if (arr[i] > arr[j]) {
+                        temp = arr[j];
+                        arr[j] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
 
         System.out.printf("Array sorts from min to max : %s", Arrays.toString(arr));
     }
